201d89767b787a3e7daa13f4eadd829f9260a66d
jrente <jrenteria003@ikasle.ehu.eus>
2020-02-04 20:40:43 +0100

    change some configs, font maybe?

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=22:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -24,7 +24,7 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.1;
+static float cwscale = 1.0;
 static float chscale = 1.0;
 
 /*
